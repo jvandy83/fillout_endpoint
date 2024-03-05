aaaa7838e3567a61e717a178e8ceb64cc9e64fdb
jvandy83 <vanthedev@gmail.com>
2024-03-05 10:04:42 -0700

    added dotenv

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const apiKey = process.env.API_KEY;
+
 // Define endpoint for fetching filtered responses
 app.get("/:formId/filteredResponses", async (req, res) => {
   try {
     const formId = req.params.formId;
     console.log(formId);
-    const apiKey =
-      "sk_prod_TfMbARhdgues5AuIosvvdAC9WsA5kXiZlW8HZPaRDlIbCpSpLsXBeZO7dCVZQwHAY3P4VSBPiiC33poZ1tdUj2ljOzdTCCOSpUZ_3912";
 
     // Parse and validate filters from query parameters
     const filters = req.query.filters ? JSON.parse(req.query.filters) : [];
